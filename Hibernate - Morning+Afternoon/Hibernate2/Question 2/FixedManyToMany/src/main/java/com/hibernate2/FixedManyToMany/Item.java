f1dd565f3d071aedbeacb61a56eb2ef29ca089c7
Meet Shah <78909602+meetshah-accolite@users.noreply.github.com>
2021-02-28 23:19:56 +0530

    Add files via upload

---
+++ Hibernate - Morning+Afternoon/Hibernate2/Question 2/FixedManyToMany/src/main/java/com/hibernate2/FixedManyToMany/Item.java
@@ -0,0 +1,47 @@
+package com.hibernate2.FixedManyToMany;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "item")
+public class Item {
+	@Id
+	@Column(name = "itemid")
+	private int itemId;
+	@Column(name = "itemname", length = 10)
+	private String itemName;
+	@ManyToMany(targetEntity = Categories.class,  cascade = CascadeType.ALL, mappedBy="items")
+	private Set<Categories> categories;
+	public Set<Categories> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(Set<Categories> categories) {
+		this.categories = categories;
+	}
+
+	public int getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+}
